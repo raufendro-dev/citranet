2c4efd05d813d53dd9dcd7015b6f49833cd67c19
Rauf Endro Widagdo <raufendrowidagdo@MacBook-Air-Rauf.local>
2022-11-20 14:46:04 +0700

    update

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -19,57 +19,83 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   _HomeScreenState({required this.dataUser});
   var dataUser;
+  int _selectedIndex = 0;
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+      if (index == 0) {
+      } else if (index == 1) {
+      } else if (index == 2) {}
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30),
-                    bottomRight: Radius.circular(30)),
-                child: Image.asset('assets/image/kotak.png'),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 60, left: 15, right: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Halo, ' + dataUser.nama,
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Container(
-                      height: 48,
-                      width: 48,
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.all(Radius.circular(25)),
-                          child: Image.memory(
-                            base64Decode(dataUser.gambar),
-                          )),
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30)),
+                  child: Image.asset('assets/image/kotak.png'),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 100, left: 15, right: 20),
-                child: Text(
-                  'Pelanggan ID : ' + dataUser.pelangganid.toString(),
-                  style: TextStyle(color: Colors.white, fontSize: 15),
+                Padding(
+                  padding: EdgeInsets.only(top: 60, left: 15, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Halo, ' + dataUser.nama,
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Container(
+                        height: 48,
+                        width: 48,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.all(Radius.circular(25)),
+                            child: Image.memory(
+                              base64Decode(dataUser.gambar),
+                            )),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          )
-        ],
+                Padding(
+                  padding: EdgeInsets.only(top: 100, left: 15, right: 20),
+                  child: Text(
+                    'Pelanggan ID : ' + dataUser.pelangganid.toString(),
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
-    ));
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.announcement), label: "Aduan"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: "Rumah Sakit"),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+      ),
+    );
   }
 }
